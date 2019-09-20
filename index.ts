801afd7e07f647f332b6f71e3bc71330daa9fe64
david ma <dmlh02@gmail.com>
2019-09-20 11:40:50 +0100

    Upload changes.

---
+++ index.ts
@@ -0,0 +1,24 @@
+
+
+import { terminal as term  } from 'terminal-kit';
+term.black.bgWhite("black");
+term.red(" red ");
+term.green("green ");
+term.yellow("yellow ");
+term.blue.bgWhite("blue");
+term.magenta(" magenta ");
+term.cyan("cyan ");
+term.white("white ");
+term.brightBlack.bgWhite("bright black");
+term.brightRed(" bright red ");
+term.brightGreen("bright green ");
+term.brightYellow("bright yellow ");
+term.brightBlue("bright blue ");
+term.brightMagenta("bright magenta ");
+term.brightCyan("bright cyan ");
+term.brightWhite("bright white\n");
+term.bold( 'The terminal size is %dx%d\n' , term.width , term.height ) ;
+//term.moveTo( 1 , 1 , 'Upper-left corner' ) ;
+//term.moveTo.cyan( 1 , 1 , "My name is %s, I'm %d.\n" , 'Jack' , 32  ) ; 
+
+
