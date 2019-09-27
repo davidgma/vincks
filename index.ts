a51a13d3dedc5732e005e7e265a940411c4729af
david ma <dmlh02@gmail.com>
2019-09-27 18:32:33 +0100

    Upload changes.

--- index.ts
+++ index.ts
@@ -43,6 +43,7 @@ class Main {
 
   async ssr(url: string) {
     const browser = await launch({headless: true});
+		
     const page = await browser.newPage();
     await page.goto(url, {waitUntil: 'networkidle0'});
     const html = await page.content(); // serialized HTML of page DOM.
